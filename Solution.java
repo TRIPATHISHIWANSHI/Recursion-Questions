b7ab93bee645ac95d6f1ba12350afc5f3e8dfd58
SHIWANSHI TRIPATHI <112747153+TRIPATHISHIWANSHI@users.noreply.github.com>
2023-06-16 15:48:32 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    int missingNumber(int array[], int n) {
+        int expectedSum = (n * (n + 1)) / 2;
+        return findMissing(array, 0, expectedSum);
+    }
+    
+    int findMissing(int array[], int index, int expectedSum) {
+        if (index == array.length) {
+            return expectedSum;
+        }
+        
+        int currentSum = 0;
+        for (int i = index; i < array.length; i++) {
+            currentSum += array[i];
+        }
+        
+        return expectedSum - currentSum;
+    }
+}
