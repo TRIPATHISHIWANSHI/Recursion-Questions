351dad01b07396f41b6681a993b0906dc255cba7
SHIWANSHI TRIPATHI <112747153+TRIPATHISHIWANSHI@users.noreply.github.com>
2023-06-16 15:50:00 +0530

    Update and rename Solution.java to missing_Number.java

--- missing_Number.java
+++ missing_Number.java
@@ -1,4 +1,4 @@
-public class Solution {
+ class Solution {
     int missingNumber(int array[], int n) {
         int expectedSum = (n * (n + 1)) / 2;
         return findMissing(array, 0, expectedSum);
